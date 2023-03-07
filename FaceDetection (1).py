b0d300ba058900ec94c3b4c18a5b2190425c7de0
NEHASHUKLA94 <126643062+NEHASHUKLA94@users.noreply.github.com>
2023-03-07 19:20:42 +1100

    API-port changes

--- FaceDetection (1).py
+++ FaceDetection (1).py
@@ -41,4 +41,4 @@ def runFaceDetection():
     return {'success': True}
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=8080, debug=True)
+    app.run(host="localhost", port=3000, debug=True)
